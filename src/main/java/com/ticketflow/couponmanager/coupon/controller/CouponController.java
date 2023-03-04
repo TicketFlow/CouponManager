fce48f90d2abe007fa32142bd43638e0968c76f2
daniellygj <daniellygj@gmail.com>
2023-03-04 16:33:07 -0300

    correção

--- src/main/java/com/ticketflow/couponmanager/coupon/controller/CouponController.java
+++ src/main/java/com/ticketflow/couponmanager/coupon/controller/CouponController.java
@@ -2,17 +2,11 @@ package com.ticketflow.couponmanager.coupon.controller;
 
 import com.ticketflow.couponmanager.coupon.controller.dto.CouponDTO;
 import com.ticketflow.couponmanager.coupon.service.CouponService;
-import org.apache.sshd.server.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
 
 @RequestMapping("/coupon")
 @RestController
@@ -30,17 +24,5 @@ public class CouponController {
     public Mono<CouponDTO> createCoupon(@RequestBody @Validated CouponDTO coupon) {
         return couponService.createCoupon(coupon);
     }
-//    @Value("${server.port}")
-//    private String minhaProperty;
-//
-//    public void meuMetodo() {
-//        System.out.println("Valor da property: " + minhaProperty);
-//    }
-//
-//    @GetMapping("/test")
-//    public String test() {
-//        meuMetodo();
-//        return minhaProperty;
-//    }
 
 }
